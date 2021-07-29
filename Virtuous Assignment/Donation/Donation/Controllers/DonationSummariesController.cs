a940d73c38d33a3b5932ff37d6e0e60fea4a8543
Adam Valverde <adam.dorado@gmail.com>
2021-07-29 16:10:10 -0700

    small changes to help optimize based on our conversations (#1)

--- Virtuous Assignment/Donation/Donation/Controllers/DonationSummariesController.cs
+++ Virtuous Assignment/Donation/Donation/Controllers/DonationSummariesController.cs
@@ -151,40 +151,68 @@ namespace Donation.Controllers
         }
         public IActionResult DonationMetrics()
         {
+            var viewModel = new DonationMetricsViewModel();
+
+            //call the database once to get all three summations
+            var donationSummaries = _context.DonationSummaries
+                .GroupBy(x => x.DonationType, (type, group) => new { Type = type, Count = group.Count() })
+                .ToDictionary(x => x.Type, x => x.Count);
+
             //Query cash donations for count for views piechart
-            var cashPieChart = _context.DonationSummaries.Where(a => a.DonationType == "Cash").Count();
-            ViewBag.cashPieChart = cashPieChart;
+            //var cashPieChart = _context.DonationSummaries.Where(a => a.DonationType == "Cash").Count();
+            viewModel.CashTotal = donationSummaries["Cash"];
 
             //Query time donations for count for views piechart
-            var timePieChart = _context.DonationSummaries.Where(a => a.DonationType == "Time").Count();
-            ViewBag.timePieChart = timePieChart;
+            //var timePieChart = _context.DonationSummaries.Where(a => a.DonationType == "Time").Count();
+            viewModel.TimeTotal = donationSummaries["Time"];
 
             //Query item donations for count for views piechart
-            var itemPieChart = _context.DonationSummaries.Where(a => a.DonationType == "Item").Count();
-            ViewBag.itemPieChart = itemPieChart;
+            //var itemPieChart = _context.DonationSummaries.Where(a => a.DonationType == "Item").Count();
+            viewModel.ItemTotal = donationSummaries["Item"];
 
             //Query group by month, add total of donationcash value, to list. Year was included to add yearly charts.
-            var monthlyDonations = _context.DonationSummaries.Select(d => new { d.DonationDate.Year, d.DonationDate.Month, d.DonationCashValue }).GroupBy(x => new { x.Year, x.Month }, (key, group) => new { donationtotal = group.Sum(dt => dt.DonationCashValue) }).ToList();
+            //var monthlyDonations = _context.DonationSummaries
+            //    .Select(d => new { d.DonationDate.Year, d.DonationDate.Month, d.DonationCashValue })
+            //    .GroupBy(x => new { x.Year, x.Month }, (key, group) => new { donationtotal = group.Sum(dt => dt.DonationCashValue) })
+            //    .ToList();
 
-            //List to recieve decimal values from foreach loop
-            List<Decimal> donationSummaryList = new List<Decimal>();
-            //Foreach loop to convert from anonymous type list to decimal type values
-            foreach (var monthlyDonation in monthlyDonations)
+            //set a start boundary to fetch giving
+            var startDate = DateTime.Now.AddYears(-1);
+            startDate = new DateTime(startDate.Year, startDate.Month, 1);
+
+            //call the database and get the total giving for each month
+            var monthlyDonations = _context.DonationSummaries
+                .Where(x => x.DonationDate >= startDate)
+                .GroupBy(x => new { x.DonationDate.Year, x.DonationDate.Month }, (date, group) => new { Year = date.Year, Month = date.Month, Total = group.Sum(x => x.DonationCashValue) })
+                .OrderBy(x => x.Year)
+                .ThenBy(x => x.Month)
+                .ToList();
+
+            //cycle through to find and fill empty months along with data that does exist (leaves open the chance of no giving and aligns the chart)
+            for (int i = 0; i < 12; i++)
             {
-                donationSummaryList.Add(monthlyDonation.donationtotal);
+                viewModel.TimeAxis.Add(startDate.ToString("MM/yyyy"));
+                var monthDonations = monthlyDonations.SingleOrDefault(x => x.Year == startDate.Year && x.Month == startDate.Month);
+                if (monthDonations == null)
+                {
+                    viewModel.Datapoints.Add(0);
+                }
+                else
+                {
+                    viewModel.Datapoints.Add(monthDonations.Total);
+                }
+                startDate = startDate.AddMonths(1);
             }
-            //Serialization of decimal list to send to views script chart
-            ViewBag.donationSummaryList = Newtonsoft.Json.JsonConvert.SerializeObject(donationSummaryList);
 
             //Query sum of donationcashvalue to date
             var totalDonationsToDate = _context.DonationSummaries.Sum(a => a.DonationCashValue);
-            ViewBag.totalDonationsToDate = totalDonationsToDate;
+            viewModel.TotalDonationsToDate = totalDonationsToDate.ToString("C0"); //format the decimal as currency
 
             //Query max of donationcashvalue to date
             var largestDonationToDate = _context.DonationSummaries.Max(a => a.DonationCashValue);
-            ViewBag.largestDonationToDate = largestDonationToDate;
+            viewModel.LargestDonationToDate = largestDonationToDate.ToString("C0"); //format the decimal as currency
 
-            return View();
+            return View(viewModel); //modelbind the view
         }
     }
 }
---
+++ Virtuous Assignment/Donation/Donation/Models/DonationMetricsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donation.Models
+{
+    public class DonationMetricsViewModel
+    {
+        public DonationMetricsViewModel()
+        {
+            TimeAxis = new List<string>();
+            Datapoints = new List<decimal>();
+        }
+
+        public int CashTotal { get; set; }
+
+        public int TimeTotal { get; set; }
+
+        public int ItemTotal { get; set; }
+
+        public List<string> TimeAxis { get; set; }
+
+        public List<decimal> Datapoints { get; set; }
+
+        public string TotalDonationsToDate { get; set; }
+
+        public string LargestDonationToDate { get; set; }
+    }
+}
